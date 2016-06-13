cf4b86316983029f2f98a37cd63f48e2b56adab0
Damian Brady <damian.brady@octopus.com>
2016-06-14 09:17:14 +1000

    Pulled LD implementation into its own file

--- WeatherView/FeatureFlags/IFeatureFlagProvider.cs
+++ WeatherView/FeatureFlags/IFeatureFlagProvider.cs
@@ -1,28 +1,7 @@
-﻿using LaunchDarkly.Client;
-
-namespace WeatherView.FeatureFlags
+﻿namespace WeatherView.FeatureFlags
 {
     public interface IFeatureFlagProvider
     {
         bool Toggle(string flagKey, int userId, string userName);
-    }
-
-    class LaunchDarklyFeatureFlagProvider : IFeatureFlagProvider
-    {
-        private readonly LdClient _ldClient;
-
-        public LaunchDarklyFeatureFlagProvider(LdClient ldClient)
-        {
-            _ldClient = ldClient;
-        }
-
-        public bool Toggle(string flagKey, int userId, string userName)
-        {
-            var user = new User(userId.ToString())
-            {
-                Name = userName
-            };
-            return _ldClient.Toggle(flagKey, user);
-        }
     }
 }
---
+++ WeatherView/FeatureFlags/LaunchDarklyFeatureFlagProvider.cs
@@ -0,0 +1,23 @@
+﻿using LaunchDarkly.Client;
+
+namespace WeatherView.FeatureFlags
+{
+    class LaunchDarklyFeatureFlagProvider : IFeatureFlagProvider
+    {
+        private readonly LdClient _ldClient;
+
+        public LaunchDarklyFeatureFlagProvider(LdClient ldClient)
+        {
+            _ldClient = ldClient;
+        }
+
+        public bool Toggle(string flagKey, int userId, string userName)
+        {
+            var user = new User(userId.ToString())
+            {
+                Name = userName
+            };
+            return _ldClient.Toggle(flagKey, user);
+        }
+    }
+}
